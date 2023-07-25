b5d7811888971c69904c043edc963e1bf63face6
sgaeng <ksge1124@gmail.com>
2023-07-25 16:35:24 +0900

    send email router for password reset

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("mypill-list", views.MyPillList.as_view()),  # 유저의 즐겨찾기 목록 반환
     path("searchlog-list", views.SearchLogList.as_view()),  # 유저가 검색했던 알약 목록 반환
     path("change-password", views.ChangePassword.as_view()),
-    path("find-password", views.send_email), # 비밀번호 찾기위해서(구글이메일보내는 기능,test) 
+    path("reset-password", views.ResetPassword.as_view()),
+    # path("find-password", views.send_email),
 ]
