a4ba550d4a560ebf9ca710446d3582af753d0771
sgaeng <ksge1124@gmail.com>
2023-08-23 23:59:00 +0900

    add details

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -4,6 +4,8 @@ from users import views
 
 app_name = "users"
 
+# url: v1/users/
+
 urlpatterns = [
     path("mypill-list", views.MyPillList.as_view()),  # 유저의 즐겨찾기 목록 반환
     path("searchlog-list", views.SearchLogList.as_view()),  # 유저가 검색했던 알약 목록 반환
