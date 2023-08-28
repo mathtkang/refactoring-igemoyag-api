dba624552db0fd2f7b1f21bdac61bb0d8388a485
sgaeng <ksge1124@gmail.com>
2023-08-29 03:05:19 +0900

    add details

--- backend/pills/urls.py
+++ backend/pills/urls.py
@@ -8,6 +8,6 @@ app_name = "pills"
 urlpatterns = [
     path("", views.PillList.as_view()),
     path("search-direct", views.DirectSearchPillList.as_view()),  # ?name={알약이름}&color_front={앞면색상}&shape={알약모양}&page={페이지}
-    path("search-photo/", views.search_photo),
     path("<int:pnum>", views.PillDetails.as_view())
+    # path("search-photo/", views.search_photo),
 ]
